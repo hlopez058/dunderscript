e793efaa67567765a8cc518558bc3e6dcf87519f
hlopez058 <hlopez058@gmail.com>
2015-12-14 22:12:33 -0500

    Revision 3.0 Initial Commit
    Added a gui that will host javascript functions and allow the user to
    write in a shared document between other users. All scripts can then be
    loaded into priv or pub spaces.

---
+++ 3.0/dunderscript/dunderscript/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dunderscript
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            //begin initialization routine
+            LoadItemsToTree();
+
+
+        }
+
+        private void LoadItemsToTree()
+        {
+            var n = new ObjectLibrary();
+            n.LoadFiles();
+
+            tvItemObjLib.Items.Clear();
+            foreach (var i in n.ScriptFiles)
+            {
+                var item = new TreeViewItem();
+
+                item.Tag = i;
+                item.Header = i.name;
+                item.MouseDoubleClick += item_MouseDoubleClick;
+
+                tvItemObjLib.Items.Add(item);
+            }
+
+           
+        }
+
+        void item_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var tvItem = (TreeViewItem)sender;
+            var file = (scriptFile)tvItem.Tag;
+            OpenFileForEditing(file);
+        }
+
+        private void OpenFileForEditing(scriptFile file)
+        {
+            //create a new tab window
+            var tab =  new TabItem();
+            //add a rich text box
+            var rtf = new System.Windows.Forms.RichTextBox();
+            var data = System.IO.File.ReadAllText(file.path);
+            TextRange txt = new TextRange(rtfEditor.Document.ContentStart, rtfEditor.Document.ContentEnd);
+            txt.Text = "";
+            txt.Text = data;
+        }
+
+   
+    }
+
+}
---
+++ 3.0/dunderscript/dunderscript/dunderscript.FileIO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dunderscript
+{
+
+    public class ObjectLibrary 
+    {
+        public readonly ICollection<scriptFile> _ScriptFiles = new ObservableCollection<scriptFile>();
+
+        public ICollection<scriptFile> ScriptFiles 
+        {
+            get
+            {
+                return _ScriptFiles;
+            }
+        }
+
+        public void LoadFiles()
+        {
+            //open directory
+            //search files with extensions
+            var dir = System.AppDomain.CurrentDomain.BaseDirectory;
+            var ext = new List<string> { ".dnd", ".js" };
+            var myFiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories)
+                .Where(s => ext.Any(e => s.EndsWith(e)));
+            foreach (var file in myFiles)
+            {
+                var name = file.Remove(0, file.LastIndexOf('\\') + 1);
+                _ScriptFiles.Add(new scriptFile(file, name));
+            }
+        }
+
+        public ObjectLibrary()
+        {  
+            
+
+            
+        }
+
+         
+    }
+   public class scriptFile
+            {
+                string _name;
+                public string name { get { return _name; } set { _name = value; } }
+
+                string _path;
+                public string path { get { return _path; } set { _path = value; } }
+
+                public scriptFile(string path,string name)
+                {
+                    this._name = name;
+                    this._path = path;
+                }
+            }
+}
